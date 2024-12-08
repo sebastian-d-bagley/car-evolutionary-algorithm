589b6944f0722ee981a8db8b3f1c1b50b8085b49
Sebastian Bagley <109557968+sebastian-d-bagley@users.noreply.github.com>
2024-12-08 00:35:22 -0500

    Add files via upload

---
+++ Graphics/Polygon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Car_Genetic_Algorithm.Graphics
+{
+    public class Polygon : Shape
+    {
+        public int Edges { get; private set; }
+
+        public Polygon(IEnumerable<Vector2> vertices, Color color) : base(vertices, color)
+        {
+            Edges = Vertices.Count;
+        }
+    }
+}
